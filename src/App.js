be1c392b1a219ac7331a64d324257cb42fe9f725
Aitor Lancharro <aitor.lancharro@gmail.com>
2022-11-08 13:49:35 +0100

    Fre plan done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import "./App.css";
 import free from './assets/free.png'
+import { MdDone } from "react-icons/md";
 
 function App() {
   return (
     <div className="App">
       <div className="Cards">
-        <h1>Price Cards</h1>
         <div className="Free">
-          <img src={free} alt="free-plan"/>
+          <img src={free} alt="free-plan" />
           <h3>Starter</h3>
           <h2>Free</h2>
           <h4>per month</h4>
           <ul>
-            <li>Full courses library</li>
-            <li>A new daily meditation</li>
-            <li>Access to the meditation guru</li>
-            <li>Sleep podcasts and exercises</li>
-            <li>Mindfulness exercises</li>
-            <li>Guided meditations</li>
-            <li>Cooking recipes</li>
+            <li>
+              <MdDone /> Full courses library
+            </li>
+            <li>
+              <MdDone /> A new daily meditation
+            </li>
+            <li>
+              <MdDone /> Access to the meditation guru
+            </li>
+            <li className="disabled">
+              <MdDone /> Sleep podcasts and exercises
+            </li>
+            <li className="disabled">
+              <MdDone /> Mindfulness exercises
+            </li>
+            <li className="disabled">
+              <MdDone /> Guided meditations
+            </li>
+            <li className="disabled">
+              <MdDone /> Cooking recipes
+            </li>
           </ul>
           <button>Get Started</button>
         </div>
